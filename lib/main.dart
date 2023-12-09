0dc606dc30d6dbaf10fb2174ea747bf9681a3bfe
Adham Elganzoury <adham20022020@protonmail.com>
2023-12-09 11:04:17 +0200

    implement login screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:project/consts/theme_data.dart';
 import 'package:project/providers/theme_provider.dart';
 import 'package:project/root_screen.dart';
+import 'package:project/screens/auth/login.dart';
 import 'package:project/screens/inner_screens/product_details.dart';
 import 'package:project/screens/inner_screens/viewed_recently.dart';
 import 'package:project/screens/inner_screens/wishlist.dart';
@@ -27,7 +28,7 @@ class MyApp extends StatelessWidget {
           title: 'Shop Smart AR ',
           theme: Style.themeData(
               isDarkTheme: themeProvider.getIsDarkTheme, context: context),
-          home: const RootScreen(),
+          home: const LoginScreen(),
           routes: {
             productDetails.routName: (context) => const productDetails(),
             WishListScreen.routeName: (context) => const WishListScreen(),
